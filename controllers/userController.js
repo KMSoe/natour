5174e1aeca3ee093bb27000ee7a4fa04b95eea6b
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-22 19:47:44 +0630

    natour basic API

---
+++ controllers/userController.js
@@ -0,0 +1,34 @@
+exports.getAllUsers = (req, res) => {
+    res.status(500).json({
+        status: false,
+        'error': 'Somethong Went Wrong!!!',
+    });
+}
+
+exports.getUser = (req, res) => {
+    res.status(500).json({
+        status: false,
+        'error': 'Somethong Went Wrong!!!',
+    });
+}
+
+exports.createNewUser = (req, res) => {
+    res.status(500).json({
+        status: false,
+        'error': 'Somethong Went Wrong!!!',
+    });
+}
+
+exports.updateUser = (req, res) => {
+    res.status(500).json({
+        status: false,
+        'error': 'Somethong Went Wrong!!!',
+    });
+}
+
+exports.deleteUser = (req, res) => {
+    res.status(500).json({
+        status: false,
+        'error': 'Somethong Went Wrong!!!',
+    });
+}
---
+++ server.js
@@ -0,0 +1,6 @@
+const app = require('./app');
+
+const port = 8000;
+app.listen(port, () => {
+  console.log(`running on port 8000`);
+});
