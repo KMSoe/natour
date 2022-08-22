aa07f237320b788e3a86b900e37abff2c096ef6e
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-22 20:35:31 +0630

    connect to mongo db using mongoose

--- app.js
+++ app.js
@@ -1,5 +1,47 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const morgan = require('morgan');
+
+dotenv.config({ path: './config.env' });
+
+const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
+
+mongoose.connect(DB, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: true
+}).then(con => {
+  console.log(con.connections);
+  console.log('DB connection successfully');
+})
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: ['true', 'A tour have a name'],
+    unique: true
+  },
+  rating: {
+    type: Number,
+    default: 4.3
+  },
+  price: {
+    type: Number,
+    required: ['true', 'A tour have a price'],
+  }
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+  name: 'The Forest Hiker',
+  rating: 4.5,
+});
+
+Tour.find().then(res => {
+  console.log(res)
+}).catch(err => console.log('Error'))
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
