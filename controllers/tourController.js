6490802a611a8499314abaf90c2b1017ad772822
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-23 19:02:30 +0630

    fix pagination and added aliasing to tour routes

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,5 +1,13 @@
 const Tour = require('../models/tourModel');
 
+exports.aliasTopTours = (req, res, next) => {
+  req.query.limit = 5
+  req.query.sort = '-ratingsAverage,price'
+  req.query.fields = 'name,price,ratingsAverage,summary,difficulty';
+
+  next();
+}
+
 exports.getAllTours = async (req, res) => {
   try {
     // Build Query
@@ -32,7 +40,7 @@ exports.getAllTours = async (req, res) => {
     // Pagination
     const page = req.query.page * 1 || 1;
     const limit = req.query.limit * 1 || 3;
-    const skip = page * limit;
+    const skip = (page - 1) * limit;
 
     query = query.skip(skip).limit(limit);
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -3,6 +3,7 @@ const tourController = require('../controllers/tourController');
 
 const router = express.Router();
 
+router.get('/top-5-tours', tourController.aliasTopTours ,tourController.getAllTours);
 router.route('/').get(tourController.getAllTours).post(tourController.createNewTour);
 
 router
