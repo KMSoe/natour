0b556768b8c18f8ef2d7e2f89a01ed425b6147af
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-25 23:11:07 +0630

    Error Handling Middleware

--- app.js
+++ app.js
@@ -7,14 +7,16 @@ dotenv.config({ path: './config.env' });
 
 const DB = process.env.DB_URL.replace('<PASSWORD>', process.env.DB_PASSWORD);
 
-mongoose.connect(DB, {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: true
-}).then(con => {
-  console.log(con.connections);
-  console.log('DB connection successfully');
-})
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  })
+  .then((con) => {
+    console.log(con.connections);
+    console.log('DB connection successfully');
+  });
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
@@ -31,5 +33,28 @@ app.use((req, res, next) => {
 // Routes
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // return res.status(404).json({
+  //   status: false,
+  //   message: `Can't find ${req.originalUrl} on the server!!`,
+  // });
+
+  const err = new Error(`Can't find ${req.originalUrl} on the server!!`);
+  err.status = false;
+  err.statusCode = 404;
+
+  next(err);
+});
+
+app.use((err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || false;
+
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+});
 
 module.exports = app;
