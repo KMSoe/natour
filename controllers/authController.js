810091e92b7d041e01ef3aab52d6174b4fb0b1fd
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-29 20:53:33 +0630

    added restrict method for authorization

--- controllers/authController.js
+++ controllers/authController.js
@@ -55,3 +55,12 @@ exports.protect = catchAsync(async(req, res, next) => {
 
     next()
 })
+
+exports.restrictTo = (...roles) => {
+    return (req, res, next) => {
+        if(!roles.includes(req.user.role)) {
+            return next(new AppError(403, 'You don\'t have permission to do this action!!'));
+        }
+        next();
+    };
+};
