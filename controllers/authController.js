6259256c563e4e0cbfd5dc668ac7f4fcaff0609b
Kaung Myat Soe <kaungmyatsoe.m192@gmail.com>
2022-08-29 21:39:46 +0630

    added passwordResetToken in userSchema

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,64 +3,86 @@ const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 
-exports.signup = catchAsync(async(req, res, next) => {
-    const user = await User.create(req.body);
+exports.signup = catchAsync(async (req, res, next) => {
+  const user = await User.create(req.body);
 
-    const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {
-        expiresIn: process.env.JWT_EXPIRES_IN
-    });
+  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
-    return res.status(201).json({
-        status: true,
-        token,
-        data: {
-            user,
-        }
-    })
-})
+  return res.status(201).json({
+    status: true,
+    token,
+    data: {
+      user,
+    },
+  });
+});
 
-exports.signin = catchAsync(async(req, res, next) => {
-    // Check email & password provided
+exports.signin = catchAsync(async (req, res, next) => {
+  // Check email & password provided
 
-    // Check user exists with email
+  // Check user exists with email
 
-    // Check password correct or not
+  // Check password correct or not
 
-    return res.status(200).json({
-        status: true,
-        token,
-        data: {
-            user,
-        }
-    })
-})
+  return res.status(200).json({
+    status: true,
+    token,
+    data: {
+      user,
+    },
+  });
+});
 
-exports.protect = catchAsync(async(req, res, next) => {
-    let token;
+exports.protect = catchAsync(async (req, res, next) => {
+  let token;
 
-    // 1. Getting token and check if it's there
-    if(req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
-        token = req.headers.authorization.split(' ')[1];
-    }
+  // 1. Getting token and check if it's there
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    token = req.headers.authorization.split(' ')[1];
+  }
 
-    if(!token) {
-        return next(new AppError(401, 'You\'re not logged in! Please login'));
-    }
+  if (!token) {
+    return next(new AppError(401, "You're not logged in! Please login"));
+  }
 
-    // 2. Verificaton token
+  // 2. Verificaton token
 
-    // 3. Check if user still exists
+  // 3. Check if user still exists
 
-    // 4. Check if user changed password after the token issued
+  // 4. Check if user changed password after the token issued
 
-    next()
-})
+  next();
+});
 
 exports.restrictTo = (...roles) => {
-    return (req, res, next) => {
-        if(!roles.includes(req.user.role)) {
-            return next(new AppError(403, 'You don\'t have permission to do this action!!'));
-        }
-        next();
-    };
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(
+        new AppError(403, "You don't have permission to do this action!!")
+      );
+    }
+    next();
+  };
 };
+
+exports.protect = catchAsync(async (req, res, next) => {
+  // 1. Getting Ussr based on Posted email
+  let user = await User.findOne({ email: req.body.email });
+
+  if (!user) {
+    return next(new AppError(404, 'There is no user with this email address'));
+  }
+
+  // 2. Generate Random Reset token
+  const resetToken = user.createPasswordResetToken();
+  await user.save({ validateBeforeSave: false });
+
+  // 3. Send it to user's email
+
+  next();
+});
--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
@@ -34,7 +35,10 @@ const userSchema = new mongoose.Schema({
             return el == this.password;
         },
         message: 'Passwords are not the same!'
-    }
+    },
+    passwordChangedAt: Date,
+    passwordResetToken: String,
+    passwordResetExpires: Date,
   }
 });
 
@@ -42,9 +46,18 @@ userSchema.pre('save', async function(next) {
   if(!this.isModified('password')) return next();
   this.password = await bcrypt.hash(this.password, 12)
 
-  this.passwordConfirm = undefined;;
+  this.passwordConfirm = undefined;
   next();
 })
+
+userSchema.methods.createPasswordResetToken = function() {
+    const resetToken = crypto.randomBytes(32).toString('hex');
+
+  this.passwordResetToken = crypto.createHash('sha256').update(resetToken).digest('hex');
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+}
 
 const User = mongoose.model('User', userSchema);
 
